68b38f761e7f57204076fd44fce79a462d8eda85
Justin Kouwenhoven <justin.kouwenhoven@gmail.com>
2016-07-18 20:18:56 +0200

    Implemented auto reload if no results where found due to errors etc.

--- index.php
+++ index.php
@@ -1,72 +1,74 @@
 <?php
 
-if ($_POST) {
-    set_time_limit(0);
+include_once 'config.php';
 
-    if(isset($_POST['get'])){
-        $outputNot = true;
-        while ($outputNot) {
-            $output = shell_exec('cd python; python main.py -u "'.USERNAME.'" -p "'.PASSWORD.'" -l "' . $_POST['address'] . '" > data/results_$(date +"%Y%m%d_%H%M%S").txt &');
-            if (strpos($output, 'Traceback') === false) {
-                $outputNot = false;
-                echo "<pre>";
-                print_r($output);
-                echo "</pre>";
-            }
-            sleep(5);
-        }
+$address = isset($_POST['address']) ? $_POST['address'] : '';
+$address = isset($_GET['address']) ? urldecode($_GET['address']) : $address;
+$addressEncode = urlencode($address);
+
+set_time_limit(0);
+
+if (isset($_POST['get']) || isset($_GET['get'])) {
+    shell_exec('cd python; python main.py -u "' . USERNAME . '" -p "' . PASSWORD . '" -l "' . $address . '" > data/results_$(date +"%Y%m%d_%H%M%S").txt &');
+    sleep(20);
+    header('Location: ?load=true&address=' . $addressEncode);
+    exit;
+}
+
+if (isset($_POST['load']) || isset($_GET['load'])) {
+
+    $data = array();
+
+    $files = array();
+
+    foreach (glob(dirname(__FILE__) . "/python/data/*.*") as $filename) {
+        $files[] = $filename;
     }
 
-    if(isset($_POST['load'])){
+    $files = array_slice($files, -1);
 
-        $data = array();
+    foreach ($files as $file) {
+        $handle = fopen($filename, "r");
+        if ($handle) {
+            while (($line = fgets($handle)) !== false) {
 
-        $files = array();
+                if (strpos($line, 'is visible at (') !== false) {
+                    $found = $line;
 
-        foreach (glob(dirname(__FILE__)."/python/data/*.*") as $filename) {
-            $files[] = $filename;
-        }
+                    $explodeSpace = explode(' ', $found);
 
-        $files = array_slice($files, -1);
+                    $explode = explode('is visible at (', $found);
+                    $explode2 = explode(') for ', $explode[1]);
+                    $explode3 = explode(' seconds', $explode2[1]);
 
-        foreach($files as $file){
-            $handle = fopen($filename, "r");
-            if ($handle) {
-                while (($line = fgets($handle)) !== false) {
+                    $explodeLatLng = explode(', ', $explode2[0]);
 
-                    if (strpos($line, 'is visible at (') !== false) {
-                        $found = $line;
+                    $latlng = $explode2[0];
 
-                        $explodeSpace = explode(' ', $found);
+                    $seconds = $explode3[0];
 
-                        $explode = explode('is visible at (', $found);
-                        $explode2 = explode(') for ', $explode[1]);
-                        $explode3 = explode(' seconds', $explode2[1]);
+                    $number = $explodeSpace[0];
+                    $name = $explodeSpace[1];
 
-                        $explodeLatLng = explode(', ', $explode2[0]);
-
-                        $latlng = $explode2[0];
-
-                        $seconds = $explode3[0];
-
-                        $number = $explodeSpace[0];
-                        $name = $explodeSpace[1];
-
-                        $data[] = array(
-                            'number' => $number,
-                            'name' => $name,
-                            'lat' => $explodeLatLng[0],
-                            'lng' => $explodeLatLng[1],
-                            'seconds' => $seconds,
-                        );
-
-                    }
+                    $data[] = array(
+                        'number' => $number,
+                        'name' => $name,
+                        'lat' => $explodeLatLng[0],
+                        'lng' => $explodeLatLng[1],
+                        'seconds' => $seconds,
+                    );
 
                 }
 
-                fclose($handle);
             }
+
+            fclose($handle);
         }
+    }
+
+    if (empty($data)) {
+        header('Location: ?get=true&address=' . $addressEncode);
+        exit;
     }
 }
 
@@ -75,17 +77,17 @@ if ($_POST) {
 <!DOCTYPE html>
 <html>
 <head>
-    <script src="http://maps.googleapis.com/maps/api/js?key=<?php echo GOOGLE_MAPS_API_KEY;?>"></script>
+    <script src="http://maps.googleapis.com/maps/api/js?key=<?php echo GOOGLE_MAPS_API_KEY; ?>"></script>
     <script>
         var oldMarker;
         var map;
         var infowindow;
         var pokemons = <?php echo !empty($data) ? json_encode($data) : "''";?>;
-        var myLocation = <?php echo !empty($_POST['address']) ? "'".$_POST['address']."'" : "''";?>;
+        var myLocation = <?php echo !empty($address) ? "'" . $address . "'" : "''";?>;
 
         function initialize() {
 
-            if(myLocation){
+            if (myLocation) {
                 setAddressMarker(myLocation);
             }
 
@@ -101,18 +103,18 @@ if ($_POST) {
                 content: ''
             });
 
-            if(!myLocation){
+            if (!myLocation) {
                 placeMarker(pos, 'You are here!');
                 currentLocation();
             }
 
-            google.maps.event.addListener(map, 'click', function(event) {
+            google.maps.event.addListener(map, 'click', function (event) {
                 placeMarker(event.latLng, 'You are here!');
                 getAddressLatLng(event.latLng.lat(), event.latLng.lng());
             });
 
-            if(pokemons){
-                var infowindow =  new google.maps.InfoWindow({
+            if (pokemons) {
+                var infowindow = new google.maps.InfoWindow({
                     content: ""
                 });
 
@@ -127,10 +129,10 @@ if ($_POST) {
                         position: latLng,
                         map: map,
                         title: pokemon.name,
-                        icon : 'http://pokeapi.co//media//sprites//pokemon//'+cleanNumber+'.png'
+                        icon: 'http://pokeapi.co//media//sprites//pokemon//' + cleanNumber + '.png'
                     });
 
-                    bindInfoWindow(marker, map, infowindow, 'Nr: '+pokemon.number+ ' | Name: '+pokemon.name+ ' | Seconds: '+pokemon.seconds);
+                    bindInfoWindow(marker, map, infowindow, 'Nr: ' + pokemon.number + ' | Name: ' + pokemon.name + ' | Seconds: ' + pokemon.seconds);
                 }
             }
 
@@ -138,7 +140,7 @@ if ($_POST) {
 
         google.maps.event.addDomListener(window, 'load', initialize);
 
-        function currentLocation(){
+        function currentLocation() {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(function (position) {
                     var pos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
@@ -159,12 +161,12 @@ if ($_POST) {
                 title: title
             });
 
-            if (oldMarker !== undefined){
+            if (oldMarker !== undefined) {
                 oldMarker.setMap(null);
             }
             oldMarker = marker;
 
-            google.maps.event.addListener(oldMarker, 'click', function(){
+            google.maps.event.addListener(oldMarker, 'click', function () {
                 infowindow.setContent(title);
                 infowindow.open(map, oldMarker);
             });
@@ -173,7 +175,7 @@ if ($_POST) {
         }
 
         function bindInfoWindow(marker, map, infowindow, description) {
-            marker.addListener('click', function() {
+            marker.addListener('click', function () {
                 infowindow.setContent(description);
                 infowindow.open(map, this);
             });
@@ -181,14 +183,14 @@ if ($_POST) {
 
         function setAddressMarker(address) {
             var geocoder = new google.maps.Geocoder();
-            geocoder.geocode( { 'address' : address }, function( results, status ) {
-                if( status == google.maps.GeocoderStatus.OK ) {
+            geocoder.geocode({'address': address}, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
                     placeMarker(results[0].geometry.location, 'You are here!');
                 }
-            } );
+            });
         }
 
-        function getAddressLatLng(lat, lng){
+        function getAddressLatLng(lat, lng) {
             var xmlhttp = new XMLHttpRequest();
 
             xmlhttp.onreadystatechange = function () {
@@ -217,18 +219,21 @@ if ($_POST) {
 <body>
 
 <form method="POST">
-    <textarea id="address" name="address" rows="2" cols="50"><?php echo !empty($_POST['address']) ? $_POST['address'] : '';?></textarea></br></br>
-    <input type="submit" name="get" value="Get pokemons"> | <input type="button" value="Get Current Location" onclick="currentLocation(); return false;"> | <input type="submit" name="load" value="Load Pokemons">
+    <textarea id="address" name="address" rows="2"
+              cols="50"><?php echo !empty($address) ? $address : ''; ?></textarea></br></br>
+    <input type="submit" name="get" value="Get pokemon"> | <input type="button" value="Get Current Location"
+                                                                   onclick="currentLocation(); return false;"> | <input
+        type="submit" name="load" value="Load last Pokemon">
 </form>
 </br>
 <div id="googleMap" style="width:500px;height:380px;"></div>
 
-<?php if(!empty($data)){
+<?php if (!empty($data)) {
     echo '<br/>';
-    foreach($data as $pokemon){
-        echo '<p style="margin-top: 0px; margin-bottom: 0px;">Nr: '.$pokemon['number'].' | Name: '.$pokemon['name'].' | Seconds: '.$pokemon['seconds'].'</p>';
+    foreach ($data as $pokemon) {
+        echo '<p style="margin-top: 0px; margin-bottom: 0px;">Nr: ' . $pokemon['number'] . ' | Name: ' . $pokemon['name'] . ' | Seconds: ' . $pokemon['seconds'] . '</p>';
     }
-}?>
+} ?>
 
 </body>
 
